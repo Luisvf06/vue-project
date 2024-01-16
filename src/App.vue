47caed13db3c6344b81a36becc623ec75a78d819
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2024-01-16 12:51:02 +0000

    vuejs

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script>
+import Block from './components/Block.vue'
+
+export default {
+  name: 'App',
+  components: {Block},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start(){
+      this.delay=1000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+
+  <main>
+   <h1> Ninja Reaction Timer</h1>
+   <button id="boton" @click="start" :disabled="isPlaying">Play</button>
+   <Block v-if="isPlaying" :delay="delay"/>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class='block' v-if="showBlock" @click="stopTimer">
+    click me
+    </div>
+</template>
+
+<script>
+
+    export default{
+        props: ['delay'],
+        data() {
+            return {
+                showBlock: false,
+                timer:null,
+                reactionTime:0
+        }
+    },
+        mounted() {
+            setTimeout(()=>{
+                this.showBlock=true
+                this.startTimer()
+            },this.delay)
+            },
+            methods:{
+                startTimer(){
+                    this.timer=setInterval(()=>{
+                        this.reactionTime +=10
+                    },10)
+                    },
+                    stopTimer(){
+                        clearInterval(this.timer)
+                    }
+                }
+            }
+            console.log('component mounted')
+            setTimeout(()=> {
+                this.showBlock = true
+                console.log(this.delay)
+            },this.delay)
+        },
+        updated(){
+
+        }
+    }
+</script>
+<style>
+    .block{
+        width: 400px;
+        border-radius: 20px;
+        color:red;
+        text-align:center;
+        background-color:blue;
+    }
+</style>
